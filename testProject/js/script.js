6a6a18f718b7e64ea6f4a234185151c4a01d4b53
Alexander <alxpro590@gmail.com>
2021-09-12 01:26:37 +0300

    Object

--- testProject/js/script.js
+++ testProject/js/script.js
@@ -1,19 +1,54 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
+"use strict"
+
+
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: { },
+    count: null,
+    movies: {},
     actors: {},
     genres: [],
-    privat: false    
-    
+    privat: false,
+
+    quiz: function(){ 
+
+        const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
+
+        if(numberOfFilms > 0){
+
+            if(numberOfFilms >= 30){
+                alert("Побереги глаза бро!");
+            } else if (numberOfFilms >= 10) {
+                alert("Отлично, ты середнячок!");
+            } else if(numberOfFilms > 0) {
+                alert("Ты совсем не смотришь фильмы, нужно исправляться!");
+            } 
+
+            this.count = numberOfFilms;
+
+            const quest = ['Название последнего фильма?', 'На сколько его оцените'];
+            let answ = ['', ''];
+
+            for (let i = 0; i < 2; i++){
+                for (let nQ = 0; nQ < 2;){                
+                    answ[nQ] = prompt((i + 1) + '. ' +  quest[nQ], '');
+                        
+                    if ((answ[nQ] != null) && (answ[nQ].length > 0 && answ[nQ].length < 50)){
+                        nQ++;
+                    }
+                }
+                    this.movies[answ[0]] = answ[1];
+                    answ = ['', ''];
+            }
+        } else {
+            this.count = 0;
+            alert("Опять трололо, нолик тебе в поле!");
+        }
+    } 
 };
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?', ''), 
-       raiting = prompt('На сколько его оцените', ''),
-       lastMovie_1 = prompt('Один из последних просмотренных фильмов?', ''), 
-       raiting_1 = prompt('На сколько его оцените', '');
+personalMovieDB.quiz();
+
+console.log(personalMovieDB);
 
 
-       personalMovieDB.movies[lastMovie] = raiting;
-       personalMovieDB.movies[lastMovie_1] = raiting_1;
+
