cd51f43981bd80fa95bbf02b202857ee2bf95243
Alexander <alxpro590@gmail.com>
2021-09-11 22:40:21 +0300

    First Step

--- testProject/js/script.js
+++ testProject/js/script.js
@@ -1,15 +1,19 @@
-"use strict"
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
 
-let dddd = prompt('Сукер' ,"Нах");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: { },
+    actors: {},
+    genres: [],
+    privat: false    
+    
+};
+
+const lastMovie = prompt('Один из последних просмотренных фильмов?', ''), 
+       raiting = prompt('На сколько его оцените', ''),
+       lastMovie_1 = prompt('Один из последних просмотренных фильмов?', ''), 
+       raiting_1 = prompt('На сколько его оцените', '');
 
 
-
-function яёбнутачфункция(){
-    let a = 'eke.eke.eke.';
-}
-
-
-let ty = 'sds';
-ty = +ty;
-
-console.log(ty);
+       personalMovieDB.movies[lastMovie] = raiting;
+       personalMovieDB.movies[lastMovie_1] = raiting_1;
